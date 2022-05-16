ea1691e51f15ed9773393906ec060a90a8ed4061
vodangkhoa2001 <vdk0610@gmail.com>
2022-05-16 22:27:26 +0700

    add laravel

---
+++ public/js/java.js
@@ -0,0 +1,8 @@
+function kiemtra()
+{
+    var cm=document.getElementById('noidungbinhluan').value;
+    if(cm=="")
+    {
+        document.getElementById('kiemtrabinhluan').innerHTML="Bình luận trống.";
+    }
+}
